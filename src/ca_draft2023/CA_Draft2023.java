c7e42f3490c54d644679dacf5d65b047b512c2d4
C2022188 <2022188@student.cct.ie>
2023-12-09 13:19:10 +0000

    Update CA_Draft2023.java

--- src/ca_draft2023/CA_Draft2023.java
+++ src/ca_draft2023/CA_Draft2023.java
@@ -1,156 +1,160 @@
-        /*
-         * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-         * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
-         */
-        package ca_draft2023;
+            /*
+             * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+             * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+             */
+            package ca_draft2023;
 
-        import ioutils.IOUtils;
-        import java.sql.SQLException;
-        import java.util.ArrayList;
-        import java.util.Scanner;
-
-        /**
-         *
-         * @author caroo
-         */
-        public class CA_Draft2023 {
+            import ioutils.IOUtils;
+            import java.sql.SQLException;
+            import java.util.ArrayList;
+            import java.util.Scanner;
 
             /**
-             * @param args the command line arguments
-             * @throws java.sql.SQLException
-             * @throws java.lang.ClassNotFoundException
-             * @throws java.lang.InstantiationException
-             * @throws java.lang.IllegalAccessException
+             *
+             * @author caroo
              */
-                public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+            public class CA_Draft2023 {
 
-                  Scanner system = new Scanner(System.in);
+                /**
+                 * @param args the command line arguments
+                 * @throws java.sql.SQLException
+                 * @throws java.lang.ClassNotFoundException
+                 * @throws java.lang.InstantiationException
+                 * @throws java.lang.IllegalAccessException
+                 */
+                    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 
-                    System.out.println("\"WELCOME TO YOUR REVENUE TAX CALCULATION!\n" +
-            "CHOOSE AN OPTION:\n" +
-            "1: LOGIN\n" +
-            "2: SIGN UP");
+                      Scanner system = new Scanner(System.in);
 
-
-        int optionNum = system.nextInt();
-
-                    switch (optionNum) {
-                        case 1:
-                            login();
-                            break;
-                        case 2:
-                            if (!userExists()) {
-                                newUser();
-                            } else {
-                                System.out.println("User already exists in the database.");
-                            }   break;
-                        default:
-                            System.out.println("Error!.");
-                            break;
-                    }
-                }
+                        System.out.println("\"WELCOME TO YOUR REVENUE TAX CALCULATION!\n" +
+                "CHOOSE AN OPTION:\n" +
+                "1: LOGIN\n" +
+                "2: SIGN UP");
 
 
-                public static boolean userExists() {
-                    DataBaseReaderDraft dbr = new DataBaseReaderDraft();
-                    IOUtils newUserInput = new IOUtils();
+            int optionNum = system.nextInt();
 
-                    String username = newUserInput.getUserText("Choose a username: ");
-
-                    ArrayList<Users> usersList = dbr.getAllUsers();
-                    for (Users user : usersList) {
-                        if (user.getUsername().equals(username)) {
-                            return true; // User already exists
+                        switch (optionNum) {
+                            case 1:
+                                login();
+                                break;
+                            case 2:
+                                if (!userExists()) {
+                                    newUser();
+                                } else {
+                                    System.out.println("User already exists in the database. Please choose a different username.");
+                                }   break;
+                            default:
+                                System.out.println("Error!.");
+                                break;
                         }
                     }
-                    return false; // User not founded
-                }
 
 
-                public static boolean login() {
+                    public static boolean login() {
+                        
+                        //***********Aqui precisamos validar o Login, e verificar se esta correto username e password *****************
 
-                    IOUtils newUserInput = new IOUtils();
+                        IOUtils newUserInput = new IOUtils();
 
-                    boolean usertype;
-                    String username;
-                    String password;
+                       // boolean usertype;
+                        String username;
+                        String password;
 
-                    Scanner UserInput = new Scanner(System.in);
-                    System.out.println("User Type: ADMIN / REGULAR USER ");
-                    usertype = UserInput.nextBoolean();
+                        Scanner UserInput = new Scanner(System.in);
+    //                    System.out.println("User Type: ADMIN / REGULAR USER ");
+    //                    usertype = UserInput.nextBoolean();
 
-                    username = newUserInput.getUserText("Choose a new username: ");
-                    password = newUserInput.getUserText("Choose a password: ");
+                        username = newUserInput.getUserText("Username: ");
+                        password = newUserInput.getUserText("Password: ");
+                        
 
-                    return true;
-
-                }
-
-                    public static void newUser() throws SQLException {
-
-                    Scanner UserInput = new Scanner(System.in);
-
-                    String firstName;
-                    String lastName;
-                    String email;
-                    String PPSN;
-                    double grossIncome;
-                    double taxCredit;
-//                    String usertype;
-                    String username;
-                    String password;
-
-
-                    System.out.println("First Name: ");
-                    firstName = UserInput.nextLine();
-           if (userExists()) {
-        System.out.println("User already exists in the database. Please choose a different username.");
-        return;}
-                    
-                    System.out.println("Last Name: ");
-                    lastName = UserInput.nextLine();
-                    
-                    System.out.println("Email: ");
-                    email = UserInput.nextLine();
-                    
-                    System.out.println("PPSN: ");
-                    PPSN = UserInput.nextLine();
-                    
-                    System.out.println("grossIncome: ");
-                    grossIncome = UserInput.nextDouble();
-                    UserInput.nextLine();
-                     
-                    System.out.println("Tax Credit: ");
-                    taxCredit = UserInput.nextDouble();
-                    UserInput.nextLine();
-
-//                    System.out.println("User Type: ADMIN / REGULAR USER ");
-//                    usertype = UserInput.nextLine();
-             
-                    
-                    System.out.println("New Username: ");
-                    username = UserInput.nextLine();
-                    
-                    System.out.println("Password: ");
-                    password = UserInput.nextLine();
-
-                    Users newUser = new Users (0, firstName, lastName, email, PPSN, grossIncome, taxCredit, 0, username, password);
-                    addToDatabase(newUser);
+                        return true;
 
                     }
+                    
+                    public static boolean userExists() {
+                        DataBaseReaderDraft dbr = new DataBaseReaderDraft();
+                        IOUtils newUserInput = new IOUtils();
 
+                        String username = newUserInput.getUserText("Type your a username: ");
 
-                    public static void addToDatabase(Users newUser) throws SQLException {
-                    DataBaseWriterDraft dbw = new DataBaseWriterDraft();
-                    boolean success = dbw.newUser(newUser);
-
-                    if (success) {
-                        System.out.println("New user add.");
-                    } else {
-                        System.out.println("Error.");
+                        ArrayList<Users> usersList = dbr.getAllUsers();
+                        for (Users user : usersList) {
+                            if (user.getUsername().equals(username)) {
+                                return true; // User already exists
+                            }
+                        }
+                        return false; // User not founded
                     }
-                }
+
+                        public static void newUser() throws SQLException {
+
+                        Scanner UserInput = new Scanner(System.in);
+
+                        String firstName;
+                        String lastName;
+                        String email;
+                        String PPSN;
+                        double grossIncome;
+                        double taxCredit;
+    //                    String usertype;
+                        String username;
+                        String password;
+
+
+                        System.out.println("First Name: ");
+                        firstName = UserInput.nextLine();
+
+
+                        System.out.println("Last Name: ");
+                        lastName = UserInput.nextLine();
+
+                        System.out.println("Email: ");
+                        email = UserInput.nextLine();
+
+                        System.out.println("PPSN: ");
+                        PPSN = UserInput.nextLine();
+
+                        System.out.println("grossIncome: ");
+                        grossIncome = UserInput.nextDouble();
+                        UserInput.nextLine();
+
+                        System.out.println("Tax Credit: ");
+                        taxCredit = UserInput.nextDouble();
+                        UserInput.nextLine();
+
+    //                    System.out.println("User Type: ADMIN / REGULAR USER ");
+    //                    usertype = UserInput.nextLine();
+
+
+                        System.out.println("New Username: ");
+                        username = UserInput.nextLine();
+
+                        System.out.println("Password: ");
+                        password = UserInput.nextLine();
+
+                        Users newUser = new Users (0, firstName, lastName, email, PPSN, grossIncome, taxCredit, 0, username, password);
+                        
+ 
+                    
+                        addToDatabase(newUser);
+                        
+                        }
+
+
+
+                        public static void addToDatabase(Users newUser) throws SQLException {
+                        DataBaseWriterDraft dbw = new DataBaseWriterDraft();
+                        boolean success = dbw.newUser(newUser);
+
+                        if (success) {
+                            System.out.println("Sucess - New user add.");
+                        } else {
+                            System.out.println("Error.");
+                        }
                     }
+                        }
 
 
 
