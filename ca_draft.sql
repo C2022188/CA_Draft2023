c51d9dfec12449521962b668ee1a76169521de0a
C2022188 <2022188@student.cct.ie>
2023-12-13 15:17:32 +0000

    Create ca_draft.sql

---
+++ ca_draft.sql
@@ -0,0 +1,22 @@
+/*
+-- Query: SELECT * FROM ca_draft.usersdata_draft
+LIMIT 0, 1000
+
+-- Date: 2023-12-13 15:16
+*/
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (0,'Caroline','Ferreira','carool.alvesf@gmail.com','123456789W',50000.00,1000.00,0.00,'Caroline','ahxjd');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (2,'Emerson','Barbirato','emersonbarbirato@gmail.com','1235689W',52000.00,1500.00,0.00,'Emerson','hdasjdsb');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (3,'Carlos','Ambrosio','carlosambrosio@gmail.com','466753332W',50000.00,1000.00,0.00,'Carlos','asdfghj');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (4,'Carolina','Alves','carolalves@hotmail.com','123456878W',50000.00,2000.00,0.00,'Carol','asdfghjkl;');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (5,'Carlos','Daniel','carlosdaniel@gmail.com','52369712W',50000.00,1000.00,0.00,'CarlosDaniel','asdfghjkl;');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (6,'Thiago','Golvalves','Thiago','2345678A',40000.00,1000.00,0.00,'Thiago','asdfghjkl;');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (7,'Douglas','Silva','douglas@gmail.com','234567898E',60000.00,2000.00,0.00,'Douglas','dfghjk');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (8,'Joao','Silva','joaosilva@gmail.com','852741963W',50000.00,1000.00,0.00,'Joao1','sdfghjkl;');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (9,'Andre','Felipe','andre@gmail.com','7418529632',50000.00,1.00,0.00,'Andre1','sdfghjkl;');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (10,'Kamila','Pires','kamila@gmail.com','741852963S',50000.00,0.00,0.00,'Kamila1','azsxdcfvgbhnjmk');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (11,'Maria','Carolina','carol@gmail.com','456789123W',50000.00,0.00,0.00,'Maria','asdfghjk');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (12,'Layene','Alves','layene@gmail.com','7419634562',50000.00,0.00,0.00,'Layene','sdfghjk');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (13,'Stefania','Reis','ste@gmail.com','74145696385',50000.00,0.00,2587.15,'Stef','asdfg');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (14,'Erica','Almeida','erica@gmail.com','852963456',50000.00,3550.00,2587.15,'Erica','poiuyt');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (15,'Alberto','Souza','alberto@gmail.com','963852456',50000.00,3550.00,2587.15,'Alberto','yhnujm');
+INSERT INTO `` (`id`,`firstName`,`lastName`,`email`,`PPSN`,`grossIncome`,`taxCredit`,`taxOwed`,`username`,`password`) VALUES (16,'Silvana','Siva','silva@gmail.com','741852456',40000.00,3550.00,1937.15,'Silva','tyui');
